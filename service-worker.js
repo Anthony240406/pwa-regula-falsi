086f75b0b6e8fb32326d1cbb2f264d5daca4c3a5
Anthony240406 <miguel.castaneda.v@uni.pe>
2025-05-13 11:31:13 -0500

    Describe brevemente tu cambio, p. ej. ‹Actualiza íconos PWA y configuración›

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_NAME = 'regula-falsi-cache-v1';
 const ASSETS = [
-  '.',               // equivale a index.html
-  'index.html',
-  'css/styles.css',
-  'js/app.js',
-  'js/api.js',
-  'images/icons/icon-192.png',
-  'images/icons/icon-512.png'
-];
+  './',
+  './index.html',
+  './css/styles.css',
+  './js/app.js',
+  './js/api.js',
+  './images/icons/icon-192.png',
+  './images/icons/icon-512.png'
+ ];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
