d2a3c89d5c2d7b1c3f7395cc7416ebddc297f99b
Anthony240406 <miguel.castaneda.v@uni.pe>
2025-05-13 22:25:44 -0500

    r

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ export async function calcula(N, eps) {
   const resp = await fetch('https://pwa-regula-falsi.onrender.com/api/regula', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ N, error: eps })
+    body: JSON.stringify({ N, exp: eps })
   });
   return await resp.json();
 }
